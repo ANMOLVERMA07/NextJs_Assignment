df2eac7fa6b459e99cbed50625a7e44d564ce5a2
Anmol Verma <153612130+ANMOLVERMA07@users.noreply.github.com>
2025-07-28 10:50:51 +0000

    Some changes in styling

--- my-carousel-app/src/app/page.tsx
+++ my-carousel-app/src/app/page.tsx
@@ -1,9 +1,12 @@
 import Carousel from '@/components/Carousel';
+import { FaEmpire } from 'react-icons/fa';
 
 export default function HomePage() {
   return (
     <main style={{ padding: '8rem' }}>
-      <h1 style={{ textAlign: 'center',color:"white",fontSize:'1.5rem',fontWeight:'bold' }}>The Core Values Behind our Work</h1>
+      <FaEmpire style={{ color:'orange',textAlign: 'center', scale:'3', margin:'auto',marginBottom:'2.0rem'}}/>
+      <p style={{textAlign: 'center',fontSize:'0.8rem',marginBottom:'1.2rem',color:"grey"}}><span style={{color:"orange"}}>//</span> 02 . OUR VALUES</p>
+      <h1 style={{ textAlign: 'center',color:"white",fontSize:'1.5rem',fontWeight:'bold' }}>The core values behind our work</h1>
       <Carousel />
     </main>
   );
